c75100bde906c4c92d457032bd25c2607459fecd
andgineer <Andrei_Sorokin2@epam.com>
2023-08-07 06:48:44 +0200

    https://github.com/andgineer/fastapi-playground/issues/1 user object CRUD

--- fastapi_playground/sqlmodel/main.py
+++ fastapi_playground/sqlmodel/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
-from fastapi_playground.sqlmodel.db import init_db
-from fastapi_playground.sqlmodel.models import User  # this import creates the table
+from fastapi_playground.sqlmodel.db import init_db, get_session
+from sqlalchemy import select
+from fastapi_playground.sqlmodel.models import User, UserCreate  # this import creates the table
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 app = FastAPI()
 
@@ -14,3 +16,19 @@ def on_startup():
 @app.get("/ping")
 async def pong():
     return {"ping": "pong!"}
+
+
+@app.get("/users", response_model=list[User])
+async def get_songs(session: AsyncSession = Depends(get_session)):
+    result = await session.execute(select(User))
+    users = result.scalars().all()
+    return [User(first_name=user.first_name, last_name=user.last_name, middle_name=user.middle_name, age=user.age) for user in users]
+
+
+@app.post("/users")
+async def add_song(user: UserCreate, session: AsyncSession = Depends(get_session)):
+    user = User(first_name=user.first_name, last_name=user.last_name, age=user.age, middle_name=user.middle_name)
+    session.add(user)
+    await session.commit()
+    await session.refresh(user)
+    return user
--- fastapi_playground/sqlmodel/models.py
+++ fastapi_playground/sqlmodel/models.py
@@ -3,16 +3,12 @@ from sqlmodel import SQLModel, Field
 
 class UserBase(SQLModel):
     first_name: str
-    last_name: str
+    last_name: str = Field(default=None, nullable=True)
+    middle_name: str
     age: int
 
 class User(UserBase, table=True):
-    id: int = Field(default=None, primary_key=True)
-
-    first_name: str
-    last_name: str
-    middle_name: str
-    age: int
+    id: int = Field(default=None, nullable=False, primary_key=True)
 
 
 class UserCreate(UserBase):
