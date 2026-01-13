ca2d714d858d9552f12af4ce0c1401458020b7ac
andgineer <Andrei_Sorokin2@epam.com>
2026-01-13 10:12:26 +0100

    activate now support all flavours of *conda removed platform specific hashes from environment.yml

--- activate.sh
+++ activate.sh
@@ -21,18 +21,34 @@ if ! (return 0 2>/dev/null) ; then
     exit 1  # we detected we are NOT source'd so we can use exit
 fi
 
-if [[ ! -d "$HOME/anaconda3/" ]] ; then
-  echo -e $RED"(!) Please install anaconda https://docs.anaconda.com/anaconda/install/"$NC
+# Detect conda installation (prefer Miniforge which includes mamba)
+CONDA_BASE=""
+if [[ -d "$HOME/miniforge3/" ]] ; then
+  CONDA_BASE="$HOME/miniforge3"
+  echo -e $CYAN"Using Miniforge (includes mamba)"$NC
+elif [[ -d "$HOME/mambaforge/" ]] ; then
+  CONDA_BASE="$HOME/mambaforge"
+  echo -e $CYAN"Using Mambaforge (includes mamba)"$NC
+elif [[ -d "$HOME/anaconda3/" ]] ; then
+  CONDA_BASE="$HOME/anaconda3"
+  echo -e $CYAN"Using Anaconda"$NC
+elif [[ -d "$HOME/miniconda3/" ]] ; then
+  CONDA_BASE="$HOME/miniconda3"
+  echo -e $CYAN"Using Miniconda"$NC
+else
+  echo -e $RED"(!) Please install Miniforge (recommended): https://github.com/conda-forge/miniforge"$NC
+  echo -e $RED"    Or Anaconda: https://docs.anaconda.com/anaconda/install/"$NC
   return 1  # we are source'd so we cannot use exit
 fi
 
-source "$HOME/anaconda3/bin/activate"
+source "$CONDA_BASE/bin/activate"
 conda init
 
 if conda info --envs | grep "\b${ENV_NAME}\s"; then
   echo -e $CYAN"activating environment ${ENV_NAME}"$NC
 else
-  if [[ -z $(conda list --name base | grep "^mamba ") ]]; then
+  # Only install mamba if not already available (Miniforge/Mambaforge have it built-in)
+  if ! command -v mamba &> /dev/null; then
     echo -e $CYAN"..installing mamba.."$NC
     conda install mamba>=2.0.5 --name base --channel conda-forge --yes
   fi
